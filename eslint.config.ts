db0f5e9bba688d86c3b13102642a56c51a3c8d71
edukisto <52005215+edukisto@users.noreply.github.com>
2026-06-04 19:53:26 +0300

    Add eslint.config.ts

---
+++ eslint.config.ts
@@ -0,0 +1,141 @@
+import jsonPlugin, { type JSONLanguageOptions } from '@eslint/json';
+import mdPlugin, { type MarkdownLanguageOptions } from '@eslint/markdown';
+import stylisticPlugin from '@stylistic/eslint-plugin';
+import type { Linter } from 'eslint';
+import * as mdxPlugin from 'eslint-plugin-mdx';
+import { globalIgnores } from 'eslint/config';
+import tsPlugin, { type ConfigWithExtends } from 'typescript-eslint';
+
+type tsLanguageOptions = ConfigWithExtends['languageOptions'];
+
+const json = jsonPlugin.configs.recommended;
+
+const md = mdPlugin.configs.recommended;
+
+const mdx = mdxPlugin.configs.flat;
+
+const stylistic = stylisticPlugin.configs.customize({
+  arrowParens: true,
+  blockSpacing: true,
+  braceStyle: 'stroustrup',
+  commaDangle: 'always-multiline',
+  indent: 2,
+  jsx: true,
+  quoteProps: 'as-needed',
+  quotes: 'single',
+  semi: true,
+  severity: 'error',
+});
+
+const eslintConfig: Linter.Config[] = [
+  globalIgnores([
+    '**',
+    '!{packages,xxx}/**/',
+    '!**/*.{code-workspace,js,json,jsx,md,mdx,ts,tsx}',
+    '**/{coverage,node_modules}/**',
+    '**/*.d.ts',
+  ]),
+  {
+    ...stylistic,
+    files: [
+      '**/*.{js,jsx}',
+    ],
+  },
+  {
+    ...json,
+    files: [
+      '**/*.json',
+    ],
+    language: 'json/json',
+  },
+  {
+    ...json,
+    files: [
+      '**/*.code-workspace',
+      '**/tsconfig*.json',
+    ],
+    language: 'json/jsonc',
+    languageOptions: {
+      allowTrailingCommas: true,
+    } satisfies JSONLanguageOptions,
+  },
+  ...md,
+  {
+    files: [
+      '**/*.md',
+    ],
+    language: 'markdown/gfm',
+    languageOptions: {
+      frontmatter: false,
+      math: true,
+    } satisfies MarkdownLanguageOptions,
+    rules: {
+      'markdown/no-bare-urls': 'error',
+      'markdown/no-missing-label-refs': 'warn',
+    },
+  },
+  {
+    ...mdx,
+    files: [
+      '**/*.{md,mdx}',
+    ],
+    processor: mdxPlugin.createRemarkProcessor({
+      lintCodeBlocks: true,
+      // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      // lintCodeBlocks: true,
+      // lintCodeBlocks: true,
+      // cwd: '',
+      // ignoreRemarkConfig: true,
+      // languageMapper: {},
+      // remarkConfigPath: '',
+    }),
+  },
+  {
+    ...stylistic,
+    files: [
+      '**/*.{ts,tsx}',
+    ],
+    languageOptions: {
+      parser: tsPlugin.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    } satisfies tsLanguageOptions,
+  },
+
+  /** Overrides. */
+  {
+    files: [
+      '**/package-lock.json',
+    ],
+    rules: {
+      'json/no-empty-keys': 'off',
+    },
+  },
+  {
+    files: [
+      /** See `configs.flatCodeBlocks` in `eslint-plugin-mdx`. */
+      '**/*.{md,mdx}/**/*',
+    ],
+    rules: {
+      /** Allow empty code blocks. */
+      '@stylistic/no-multiple-empty-lines': 'off',
+    },
+  },
+  {
+    files: [
+      /** See `configs.flatCodeBlocks` in `eslint-plugin-mdx`. */
+      '**/*.{md,mdx}/**/*.{ts,tsx}',
+    ],
+    languageOptions: {
+      parser: tsPlugin.parser,
+      parserOptions: {
+        projectService: false,
+      },
+    } satisfies tsLanguageOptions,
+  },
+];
+
+export {
+  eslintConfig as default,
+};
