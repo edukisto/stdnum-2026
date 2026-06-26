f115dccd9447c0cf58d14355ec46ffaf7895e4f8
edukisto <52005215+edukisto@users.noreply.github.com>
2026-06-26 20:18:41 +0300

    Fix eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,36 @@
-import jsonPlugin, { type JSONLanguageOptions } from '@eslint/json';
-import mdPlugin, { type MarkdownLanguageOptions } from '@eslint/markdown';
+import jsonPlugin, {
+  type JSONLanguageOptions as JsonLanguageOptions,
+} from '@eslint/json';
+import markdownPlugin, {
+  type MarkdownLanguageOptions,
+  type MarkdownRuleDefinition,
+} from '@eslint/markdown';
 import stylisticPlugin from '@stylistic/eslint-plugin';
 import type { Linter } from 'eslint';
 import * as mdxPlugin from 'eslint-plugin-mdx';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import tsPlugin, { type ConfigWithExtends } from 'typescript-eslint';
+import typescriptPlugin, {
+  type ConfigWithExtends as TypescriptConfig,
+} from 'typescript-eslint';
 
-type tsLanguageOptions = ConfigWithExtends['languageOptions'];
+type JsonLanguage = 'json/json' | 'json/jsonc';
+type MarkdownLanguage = 'markdown/commonmark' | 'markdown/gfm';
+type MarkdownRawRules = typeof markdownPlugin.rules;
+type MarkdownRules = {
+  readonly [K in keyof MarkdownRawRules as `markdown/${K}`]?:
+    Linter.RuleSeverity | [
+      Linter.RuleSeverity,
+      MarkdownRawRules[K] extends MarkdownRuleDefinition<{
+        RuleOptions: [infer First, ...unknown[]];
+      }> ? First : never,
+    ];
+};
+type TypescriptLanguageOptions = TypescriptConfig['languageOptions'];
 
 const stylisticConfig: Linter.Config = stylisticPlugin.configs.customize({
   arrowParens: true,
   blockSpacing: true,
-  braceStyle: 'stroustrup',
+  braceStyle: '1tbs',
   commaDangle: 'always-multiline',
   indent: 2,
   jsx: true,
@@ -25,10 +44,18 @@ const eslintConfig: Linter.Config[] = defineConfig([
   globalIgnores([
     '**',
     '!packages/**/',
-    '!**/*.{code-workspace,json,md,mdx,ts,tsx}',
+    '!**/*.{code-workspace,js,json,jsx,md,mdx,ts,tsx}',
     '**/{coverage,node_modules}/**',
     '**/*.d.ts',
   ]),
+  {
+    extends: [
+      stylisticConfig,
+    ],
+    files: [
+      '**/*.{js,jsx}',
+    ],
+  },
   {
     extends: [
       jsonPlugin.configs.recommended,
@@ -36,7 +63,10 @@ const eslintConfig: Linter.Config[] = defineConfig([
     files: [
       '**/*.json',
     ],
-    language: 'json/json',
+    ignores: [
+      '**/tsconfig*.json',
+    ],
+    language: 'json/json' satisfies JsonLanguage,
   },
   {
     extends: [
@@ -46,34 +76,56 @@ const eslintConfig: Linter.Config[] = defineConfig([
       '**/*.code-workspace',
       '**/tsconfig*.json',
     ],
-    language: 'json/jsonc',
+    language: 'json/jsonc' satisfies JsonLanguage,
     languageOptions: {
-      allowTrailingCommas: true,
-    } satisfies JSONLanguageOptions,
+      allowTrailingCommas: false,
+    } satisfies JsonLanguageOptions,
   },
   {
     extends: [
-      mdPlugin.configs.recommended,
+      /** Can't lint both inside and outside of code blocks. */
+      /** See <https://github.com/eslint/markdown/issues/276>. */
+      /** Use `eslint-plugin-mdx` for code blocks in both Markdown and MDX. */
+      markdownPlugin.configs.recommended,
     ],
     files: [
       '**/*.md',
     ],
-    language: 'markdown/gfm',
+    language: 'markdown/gfm' satisfies MarkdownLanguage,
     languageOptions: {
       frontmatter: false,
       math: true,
     } satisfies MarkdownLanguageOptions,
     rules: {
+      /** See <https://github.com/eslint/markdown#rules>. */
+      /** Non-recommended. */
+      'markdown/fenced-code-meta': 'off',
       'markdown/no-bare-urls': 'error',
-      'markdown/no-missing-label-refs': 'warn',
-    },
+      'markdown/no-duplicate-headings': ['error', { checkSiblingsOnly: true }],
+      'markdown/no-html': ['error', { allowed: [
+        'i',
+      ] }],
+      /** Recommended. */
+      'markdown/fenced-code-language': ['error', { required: [
+        'typescript',
+      ] }],
+      'markdown/heading-increment': ['error', { frontmatterTitle: '' }],
+      'markdown/no-missing-atx-heading-space': ['error', {
+        checkClosedHeadings: true,
+      }],
+      'markdown/no-missing-label-refs': ['warn', { allowLabels: [] }],
+      'markdown/no-missing-link-fragments': ['error', { ignoreCase: false }],
+      'markdown/no-multiple-h1': ['error', { frontmatterTitle: '' }],
+      'markdown/no-space-in-emphasis': ['error', { checkStrikethrough: true }],
+      'markdown/table-column-count': ['error', { checkMissingCells: true }],
+    } satisfies MarkdownRules,
   },
   {
     extends: [
       mdxPlugin.configs.flat,
     ],
     files: [
-      '**/*.{md,mdx}',
+      '**/*.mdx',
     ],
     processor: mdxPlugin.createRemarkProcessor({
       lintCodeBlocks: true,
@@ -81,23 +133,8 @@ const eslintConfig: Linter.Config[] = defineConfig([
   },
   {
     extends: [
-      tsPlugin.configs.strict,
-      tsPlugin.configs.stylistic,
-      stylisticConfig,
-    ],
-    files: [
-      /** See `configs.flatCodeBlocks` in `eslint-plugin-mdx`. */
-      '**/*.{md,mdx}/**/*.{ts,tsx}',
-    ],
-    rules: {
-      /** Allow empty code blocks. */
-      '@stylistic/no-multiple-empty-lines': 'off',
-    },
-  },
-  {
-    extends: [
-      tsPlugin.configs.strictTypeChecked,
-      tsPlugin.configs.stylisticTypeChecked,
+      typescriptPlugin.configs.strictTypeChecked,
+      typescriptPlugin.configs.stylisticTypeChecked,
       stylisticConfig,
     ],
     files: [
@@ -110,7 +147,17 @@ const eslintConfig: Linter.Config[] = defineConfig([
       parserOptions: {
         projectService: true,
       },
-    } satisfies tsLanguageOptions,
+    } satisfies TypescriptLanguageOptions,
+  },
+  {
+    extends: [
+      typescriptPlugin.configs.strict,
+      typescriptPlugin.configs.stylistic,
+      stylisticConfig,
+    ],
+    files: [
+      '**/*.{md,mdx}/**/*.{ts,tsx}',
+    ],
   },
 
   /** Overrides. */
@@ -120,6 +167,18 @@ const eslintConfig: Linter.Config[] = defineConfig([
     ],
     rules: {
       'json/no-empty-keys': 'off',
+    },
+  },
+  {
+    extends: [
+      mdxPlugin.configs.flatCodeBlocks,
+    ],
+    files: [
+      '**/*.{md,mdx}/**',
+    ],
+    rules: {
+      /** Allow empty code blocks. */
+      '@stylistic/no-multiple-empty-lines': 'off',
     },
   },
 ]);
