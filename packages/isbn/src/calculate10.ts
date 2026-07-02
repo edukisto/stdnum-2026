cde4b28525a8de93e0f8e54badde8b3722791ad1
edukisto <52005215+edukisto@users.noreply.github.com>
2026-07-03 02:31:52 +0300

    Fix calculate

--- packages/isbn/src/calculate10.ts
+++ packages/isbn/src/calculate10.ts
@@ -1,10 +1,11 @@
+const digits = '0X987654321';
+
 function calculate10(isbn: string): string {
   let sum = 0;
   for (let i = 0; i < 9; i += 1) {
     sum += (isbn.charCodeAt(i) - 48) * (10 - i);
   }
-  const mod = sum % 11;
-  return mod === 0 ? '0' : mod === 1 ? 'X' : '0123456789'.charAt(11 - mod);
+  return digits.charAt(sum % 11);
 }
 
 export {
--- packages/isbn/src/calculate13.ts
+++ packages/isbn/src/calculate13.ts
@@ -1,10 +1,11 @@
+const digits = '0987654321';
+
 function calculate13(isbn: string): string {
   let sum = 0;
   for (let i = 0; i < 12; i += 2) {
     sum += (isbn.charCodeAt(i) - 48) + (isbn.charCodeAt(i + 1) - 48) * 3;
   }
-  const mod = sum % 10;
-  return mod === 0 ? '0' : '0123456789'.charAt(10 - mod);
+  return digits.charAt(sum % 10);
 }
 
 export {
